c181603e52100db34be3390bb1d243028ed928b3
isai <isaiorellana.dev@gmail.com>
2022-04-07 09:44:54 -0600

    Revert ":heavy_plus_sign: Netlify Plugin"
    This reverts commit a078aad6d5fbf3c9367ab5a7fae6d8b67cf2354d.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const { NetlifyPlugin } = require("netlify-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -65,16 +64,6 @@ module.exports = {
       filename: "[name].css",
     }),
     new CleanWebpackPlugin(),
-    new NetlifyPlugin({
-      redirects: [
-        {
-          from: "/*",
-          to: "/index.html",
-          status: 200,
-          force: false,
-        },
-      ],
-    }),
   ],
   optimization: {
     minimize: true,
